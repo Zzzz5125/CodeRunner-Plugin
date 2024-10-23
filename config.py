6c004d378a4517c1bb92c133b8df9d9e0c7c5d0c
Zzzz5125 <112993397+Zzzz5125@users.noreply.github.com>
2024-10-23 16:44:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 		self.proxydomain = "https://code-runner-plugin5.vercel.app" # CodeRunner-Plugin 的代理服务器域名或ip，由于vercel域名或ip可能被墙，代码或图片在国内无法下载，若不需要代理则与self.domain内容相同
 		self.dbname = "Cluster0" # mongodb数据库名称,在mongodb官网注册后会赠送一个免费数据库
 		self.CORS = False # 是否开启CORS，开启则需要Referer与self.domain或self.proxydomain相同才能访问
-		self.api_url = "" # 短域名api服务器地址
+		self.api_url = "http://7dk1cvezn.mghost.site/api.php" # 短域名api服务器地址
 		#这里提供一个测试的地址，不保证稳定性与速度
 # api_url : http://7dk1cvezn.mghost.site/api.php
 # accesskey : aB3$gH8*jK6#mN1@pQ4&rT7%wZ0^cF5(iJ9)lO2!uV6*yB0#dG3&hJ8(kM1@oP4&rS7%vY0^zC5
