bf01c714aac1f3339dc3761404ac92fed46be25c
Zzzz5125 <112993397+Zzzz5125@users.noreply.github.com>
2024-10-23 16:44:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
 	def __init__(self):
-		self.domain = "https://code-runner-plugin5.vercel.app" # 部署CodeRunner-Plugin的域名或ip,要求协议完整eg.https://example.com or http://example.com
+		self.domain = "https://code-runner-plugin7.vercel.app" # 部署CodeRunner-Plugin的域名或ip,要求协议完整eg.https://example.com or http://example.com
 		self.proxydomain = "https://code-runner-plugin5.vercel.app" # CodeRunner-Plugin 的代理服务器域名或ip，由于vercel域名或ip可能被墙，代码或图片在国内无法下载，若不需要代理则与self.domain内容相同
 		self.dbname = "Cluster0" # mongodb数据库名称,在mongodb官网注册后会赠送一个免费数据库
 		self.CORS = False # 是否开启CORS，开启则需要Referer与self.domain或self.proxydomain相同才能访问
